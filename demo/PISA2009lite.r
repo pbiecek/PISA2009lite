33e04a79bff18b5fb1584692d8106cf149439c7d
pbiecek <przemyslaw.biecek@gmail.com>
2013-06-18 12:10:29 +0200

    updated demo and parent2009 help file

--- demo/PISA2009lite.r
+++ demo/PISA2009lite.r
@@ -17,3 +17,18 @@ dotchart(means, pch=19)
 # here the dictionary for CNT variable is in the PISA2009lite$CNT vectoe
 names(means) <- student2009dict$CNT[names(means)]
 dotchart(means, pch=19)
+
+# distribution of parental education level
+barplot(table(parent2009$PQMISCED), las=1)
+
+
+# merge of school and student data
+means <- unclass(by(
+  student2009[,c("PV1MATH","W_FSTUWT")], 
+  list(student2009[,"CNT"],student2009[,"SCHOOLID"]), 
+  function(x) weighted.mean(x[,1], x[,2])))
+
+country.school.means <- na.omit(as.data.frame(as.table(means)))
+boxplot(Freq~Var1, data=country.school.means, 
+        horizontal=T, las=1, at=rank(tapply(country.school.means$Freq, country.school.means$Var1, mean)), pch='.')
+
