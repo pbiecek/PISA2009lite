ed4571d480dcb5bb6b2418a2819c50490b6d4071
pbiecek <przemyslaw.biecek@gmail.com>
2013-06-26 18:26:44 +0200

    revert of accidental delete

--- demo/PISA2009lite.r
+++ demo/PISA2009lite.r
@@ -5,9 +5,9 @@ library(PISA2009lite)
 
 # Let's calculate weighted averages in MATH, based on PV1MATH for all countries
 means <- unclass(by(
-  student2009[,c("PV1MATH","W_FSTUWT")], 
-  student2009[,"CNT"], 
-  function(x) weighted.mean(x[,1], x[,2])))
+student2009[,c("PV1MATH","W_FSTUWT")],
+student2009[,"CNT"],
+function(x) weighted.mean(x[,1], x[,2])))
 
 # sort them and plot with the dotchart
 means <- sort(means)
@@ -24,11 +24,10 @@ barplot(table(parent2009$PQMISCED), las=1)
 
 # merge of school and student data
 means <- unclass(by(
-  student2009[,c("PV1MATH","W_FSTUWT")], 
-  list(student2009[,"CNT"],student2009[,"SCHOOLID"]), 
-  function(x) weighted.mean(x[,1], x[,2])))
+student2009[,c("PV1MATH","W_FSTUWT")],
+list(student2009[,"CNT"],student2009[,"SCHOOLID"]),
+function(x) weighted.mean(x[,1], x[,2])))
 
 country.school.means <- na.omit(as.data.frame(as.table(means)))
-boxplot(Freq~Var1, data=country.school.means, 
-        horizontal=T, las=1, at=rank(tapply(country.school.means$Freq, country.school.means$Var1, mean)), pch='.')
-
+boxplot(Freq~Var1, data=country.school.means,
+horizontal=T, las=1, at=rank(tapply(country.school.means$Freq, country.school.means$Var1, mean)), pch='.')
